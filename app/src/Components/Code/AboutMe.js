75a8fb9670d50265f9301b270e67d506169916f8
Soroush-Sajadi <soorooshsadjadee@gmail.com>
2020-08-11 21:19:18 +0200

    home

--- app/src/Components/Code/AboutMe.js
+++ app/src/Components/Code/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 import OpenMenu from './OpenMenu'
+import MakanOne from '../../images/Background-Pictures/Makan1.jpg'
 import '../Style/AboutMe.css';
 
 function AboutMe({ menuState, language,upDateMenuState, uppDateLanguage }) {
@@ -9,7 +10,23 @@ function AboutMe({ menuState, language,upDateMenuState, uppDateLanguage }) {
        {menuState ? 
        <OpenMenu language={language} upDateMenuState={upDateMenuState} menuState={menuState} uppDateLanguage={uppDateLanguage} />
        : 
-       <div>About me</div>}
+       <div className="About-me-wrapper">
+          <div className="profile">
+            <div className="about-me-image">
+              <img src={MakanOne} alt="Makan" />
+            </div>
+            <div className="about-me-biografi">
+              {language === 'English' ? 
+                <h3> Makan is an Irainian photografer with more than 10 years exprience. He started his career 
+                  with passion and love. To be a photografer needs both good undrestanding of art and perspective 
+                  Makan has taken pictures in many different countries
+                </h3>
+                :
+                <h3> Makan är en Iranskt fotografer med mer än 10 år erfarenhet.</h3>
+              } 
+            </div>
+          </div>
+        </div>}
     </div>
   );
 }
--- app/src/Components/Code/Home.js
+++ app/src/Components/Code/Home.js
@@ -11,13 +11,7 @@ function Home({menuState,language, upDateMenuState, uppDateLanguage}) {
     <div className="home-wrapper">
       <img src={background} />
         <h3 className="home-text">
-          mproved own provided blessing may 
-          peculiar domestic. Sight house has sex never. No visited 
-          raising gravity outward subject my cottage mr be. Hold do at 
-          tore in park feet near my case. Invitation at understood 
-          occasional sentiments insipidity inhabiting in. Off melancholy 
-          alteration principles old. Is do speedily kindness properly oh. 
-          Respect article painted cottage he is offices parlors.
+          we catch the moments
         </h3>
     </div>
     :
